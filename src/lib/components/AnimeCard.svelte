db7811647fb3a0970ef3a7bff1b08248c813023a
omargglez <omargglez@gmail.com>
2025-01-19 22:36:40 +0100

    fix: cover image size

--- src/lib/components/AnimeCard.svelte
+++ src/lib/components/AnimeCard.svelte
@@ -7,11 +7,11 @@
 
 <div
 	in:fade={{ duration: 200, delay }}
-	class="sahdow-lg row-span-4 grid grid-rows-subgrid overflow-hidden rounded-lg bg-gray-100"
+	class="sahdow-lg row-span-4 grid grid-rows-subgrid overflow-hidden rounded-lg bg-gray-100 "
 >
-	<div class="row-span-2 flex bg-gray-300">
+	<div class="row-span-2 flex bg-gray-300 max-h-100 lg:max-h-[25vh] xl:max-h-[30vh] 2xl:max-h-[40vh]">
 		<img
-			class="h-auto w-full object-contain object-center lg:object-none"
+			class="h-auto w-full object-contain object-center"
 			alt=""
 			src={anime.images.jpg.image_url}
 		/>
