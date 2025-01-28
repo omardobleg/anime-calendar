09d6bb83b82ec9a9c6b1589561647a5f65253716
omargglez <omargglez@gmail.com>
2025-01-28 12:50:01 +0100

    feat: add sticky mobile header

--- src/routes/anime/+layout.svelte
+++ src/routes/anime/+layout.svelte
@@ -22,12 +22,12 @@
 </script>
 
 <header
-	class="sticky -top-16 z-1 flex h-32 w-full items-center border-b border-gray-200 bg-gray-50 shadow-md"
+	class="sticky md:-top-16 z-1 flex md:h-32 -top-16 h-48 w-full items-center border-b border-gray-200 bg-gray-50 shadow-md"
 >
-	<div class="sticky top-0 mx-auto h-16 max-w-screen-xl px-4 sm:px-6 lg:px-8">
-		<div class="flex flex-col items-start gap-4 md:flex-row md:items-center md:justify-between">
-			<div class="inline-flex gap-2">
-				<h1 class="text-2xl font-bold text-gray-900 sm:text-3xl">Anime Page</h1>
+	<div class="sticky top-0 md:h-16 h-32   mx-auto  max-w-screen-xl px-4 md:px-6 lg:px-8">
+		<div class="flex flex-col items-start gap-1 md:gap-4 md:flex-row md:items-center md:justify-between">
+			<div class="inline-flex gap-2 p-2 items-center">
+				<h1 class="text-2xl md:text-lg lg:text-3xl font-bold text-gray-900 ">Anime Page</h1>
 				<Badge
 					onclick={filters.toggle}
 					icon={'👍'}
